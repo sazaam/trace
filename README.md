33718cb1a3c0e4bb9ab067fdd9eb4070b73ae71b
sazaam <sazaam@gmail.com>
2015-07-03 22:32:06 +0900

    trace.js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# trace
+# trace.js
+
+Simple and very humble implementation for a crowss-browser (faster-to-type) console.log method.
---
+++ trace.js
@@ -0,0 +1,6 @@
+var trace = window['trace'] = function trace(){
+	if(window.console === undefined) return arguments[arguments.length - 1] ;
+	if('apply' in console.log) console.log.apply(console, arguments) ;
+	else console.log([].concat([].slice.call(arguments))) ;
+	return arguments[arguments.length - 1] ;
+} ;
